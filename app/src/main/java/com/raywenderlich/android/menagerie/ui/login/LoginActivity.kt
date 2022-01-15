ad604f4be3384d5d9a11215ca46de39bd2b339ae
ivVane <iv.vane@gmail.com>
2022-01-16 00:07:30 +0100

    Simple value animators

--- app/src/main/java/com/raywenderlich/android/menagerie/ui/login/LoginActivity.kt
+++ app/src/main/java/com/raywenderlich/android/menagerie/ui/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.raywenderlich.android.menagerie.ui.login
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -13,33 +14,53 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity(), LoginView {
 
-  private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
-  private val loginViewModel by viewModels<LoginViewModel>()
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    private val loginViewModel by viewModels<LoginViewModel>()
 
-  companion object {
-    fun getIntent(context: Context) = Intent(context, LoginActivity::class.java).apply {
-      flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+    companion object {
+        fun getIntent(context: Context) = Intent(context, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
     }
-  }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    loginViewModel.setView(this)
-    loginViewModel.start()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loginViewModel.setView(this)
+        loginViewModel.start()
 
-    setContentView(binding.root)
-    setupUi()
-  }
-
-  private fun setupUi() {
-    binding.loginButton.setOnClickListener {
-      binding.progressBar.visibility = View.VISIBLE
-      loginViewModel.logIn()
+        setContentView(binding.root)
+        setupUi()
     }
-  }
 
-  override fun onLoggedIn() { // todo button animation, transition, progress
-    binding.progressBar.visibility = View.GONE
-    startActivity(MainActivity.getIntent(this))
-  }
+    private fun setupUi() {
+        binding.loginButton.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            loginViewModel.logIn()
+            animateLogin()
+        }
+    }
+
+    private fun animateLogin() {
+        // Making progress bar visible and changing its alpha to be fully transparent.
+        binding.progressBar.alpha = 0f
+        binding.progressBar.visibility = View.VISIBLE
+
+        val alphaAnimator = ValueAnimator.ofFloat(0f, 1f)
+        alphaAnimator.duration = 1000
+
+        alphaAnimator.addUpdateListener {
+            // Reading the current animated value as Float number.
+            val animationAlpha = it.animatedValue as Float
+
+            binding.progressBar.alpha = animationAlpha
+            binding.loginButton.alpha = 1 - animationAlpha
+        }
+
+        alphaAnimator.start()
+    }
+
+    override fun onLoggedIn() { // todo button animation, transition, progress
+        binding.progressBar.visibility = View.GONE
+        startActivity(MainActivity.getIntent(this))
+    }
 }
