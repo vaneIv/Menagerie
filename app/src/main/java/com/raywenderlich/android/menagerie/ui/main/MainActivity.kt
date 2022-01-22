2c025a99a7a863af5312c614b2683304de319807
ivVane <iv.vane@gmail.com>
2022-01-22 02:22:58 +0100

    Animating the settingsButton ImageView in MainActivity

--- app/src/main/java/com/raywenderlich/android/menagerie/ui/main/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/menagerie/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.raywenderlich.android.menagerie.ui.main
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.raywenderlich.android.menagerie.databinding.ActivityMainBinding
@@ -11,40 +13,55 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-  companion object {
-    fun getIntent(context: Context): Intent {
-      val intent = Intent(context, MainActivity::class.java)
-      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-      return intent
+    companion object {
+        fun getIntent(context: Context): Intent {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            return intent
+        }
     }
-  }
 
-  private val mainPagerAdapter by lazy { MainPagerAdapter(this, supportFragmentManager) }
+    private val mainPagerAdapter by lazy { MainPagerAdapter(this, supportFragmentManager) }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setBinding()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBinding()
+    }
 
-  private fun setBinding() {
-    val binding = ActivityMainBinding.inflate(layoutInflater)
+    private fun setBinding() {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
-    setContentView(binding.root)
-    setupUi(binding)
-  }
+        setContentView(binding.root)
+        setupUi(binding)
+    }
 
-  private fun setupUi(binding: ActivityMainBinding) {
-    val viewPager = binding.mainPager
-    val tabs = binding.tabs
-    val settingsButton = binding.settingsButton
+    private fun setupUi(binding: ActivityMainBinding) {
+        val viewPager = binding.mainPager
+        val tabs = binding.tabs
+        val settingsButton = binding.settingsButton
 
-    viewPager.adapter = mainPagerAdapter
-    tabs.setupWithViewPager(viewPager)
+        viewPager.adapter = mainPagerAdapter
+        tabs.setupWithViewPager(viewPager)
 
-    settingsButton.setOnClickListener { showSettings() }
-  }
+        settingsButton.setOnClickListener {
+            val animatable = binding.settingsButton.drawable as? Animatable2
 
-  private fun showSettings() {
-    startActivity(SettingsActivity.getIntent(this))
-  }
+            if (animatable != null) {
+                animatable.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+                    override fun onAnimationEnd(drawable: Drawable?) {
+                        showSettings()
+                        super.onAnimationEnd(drawable)
+                    }
+                })
+
+                animatable.start()
+            } else {
+                showSettings()
+            }
+        }
+    }
+
+    private fun showSettings() {
+        startActivity(SettingsActivity.getIntent(this))
+    }
 }
--- app/src/main/java/com/raywenderlich/android/menagerie/ui/petDetails/PetDetailsActivity.kt
+++ app/src/main/java/com/raywenderlich/android/menagerie/ui/petDetails/PetDetailsActivity.kt
@@ -16,56 +16,56 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PetDetailsActivity : AppCompatActivity(), PetDetailsView {
 
-  companion object {
-    private const val KEY_PET = "pet"
+    companion object {
+        private const val KEY_PET = "pet"
 
-    fun getIntent(
-      context: Context,
-      pet: Pet
-    ) = Intent(context, PetDetailsActivity::class.java).apply {
-      putExtra(KEY_PET, pet)
+        fun getIntent(
+            context: Context,
+            pet: Pet
+        ) = Intent(context, PetDetailsActivity::class.java).apply {
+            putExtra(KEY_PET, pet)
+        }
     }
-  }
 
-  private val viewModel by viewModels<PetDetailsViewModel>()
+    private val viewModel by viewModels<PetDetailsViewModel>()
 
-  private val binding by lazy { ActivityPetDetailsBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityPetDetailsBinding.inflate(layoutInflater) }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(binding.root)
-    viewModel.setView(this)
-    setupUi()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        viewModel.setView(this)
+        setupUi()
+    }
 
-  private fun setupUi() {
-    val data = intent.getSerializableExtra(KEY_PET) as? Pet ?: return
-    viewModel.setPet(data)
+    private fun setupUi() {
+        val data = intent.getSerializableExtra(KEY_PET) as? Pet ?: return
+        viewModel.setPet(data)
 
-    viewModel.petData.observe(this, {
-      val pet = it ?: return@observe
+        viewModel.petData.observe(this, {
+            val pet = it ?: return@observe
 
-      binding.petDetailsTitle.text = getString(R.string.petDetailsTitle, pet.name)
-      binding.petImage.setImageResource(pet.image)
-      binding.petName.text = pet.name
-      binding.petDescription.text = pet.description
+            binding.petDetailsTitle.text = getString(R.string.petDetailsTitle, pet.name)
+            binding.petImage.setImageResource(pet.image)
+            binding.petName.text = pet.name
+            binding.petDescription.text = pet.description
 
 
-      binding.petSleep.setImageResource(if (pet.isSleeping) R.drawable.ic_bedtime_24 else R.drawable.ic_sunny_24)
-      binding.petSleep.setOnClickListener { viewModel.onPetSleepTap(pet) }
+            binding.petSleep.setImageResource(if (pet.isSleeping) R.drawable.ic_bedtime_24 else R.drawable.ic_sunny_24)
+            binding.petSleep.setOnClickListener { viewModel.onPetSleepTap(pet) }
 
-      binding.feedPetButton.setOnClickListener {
-        viewModel.onPetFeedTap(pet)
-      }
-    })
-  }
+            binding.feedPetButton.setOnClickListener {
+                viewModel.onPetFeedTap(pet)
+            }
+        })
+    }
 
-  override fun showFeedingSleepingPetMessage() {
-    Toast.makeText(this, getString(R.string.cannotFeedSleepingPetMessage), Toast.LENGTH_SHORT)
-      .show()
-  }
+    override fun showFeedingSleepingPetMessage() {
+        Toast.makeText(this, getString(R.string.cannotFeedSleepingPetMessage), Toast.LENGTH_SHORT)
+            .show()
+    }
 
-  override fun feedPet(pet: Pet) {
-    startActivity(FeedPetActivity.getIntent(this, pet))
-  }
+    override fun feedPet(pet: Pet) {
+        startActivity(FeedPetActivity.getIntent(this, pet))
+    }
 }
