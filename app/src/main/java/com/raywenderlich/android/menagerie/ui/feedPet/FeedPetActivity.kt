03dad5b0aafe5bcf58a75c0bb3cba008b1a11650
ivVane <iv.vane@gmail.com>
2022-01-23 01:10:21 +0100

    Creating SpringAnimation for the cookie image horizontal bound

--- app/src/main/java/com/raywenderlich/android/menagerie/ui/feedPet/FeedPetActivity.kt
+++ app/src/main/java/com/raywenderlich/android/menagerie/ui/feedPet/FeedPetActivity.kt
@@ -1,10 +1,16 @@
 package com.raywenderlich.android.menagerie.ui.feedPet
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import coil.load
 import com.raywenderlich.android.menagerie.R
 import com.raywenderlich.android.menagerie.data.model.Pet
@@ -14,35 +20,76 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FeedPetActivity : AppCompatActivity() {
 
-  companion object {
-    private const val KEY_PET = "pet"
+    companion object {
+        private const val KEY_PET = "pet"
 
-    fun getIntent(context: Context, pet: Pet) = Intent(context, FeedPetActivity::class.java).apply {
-      putExtra(KEY_PET, pet)
+        fun getIntent(context: Context, pet: Pet) =
+            Intent(context, FeedPetActivity::class.java).apply {
+                putExtra(KEY_PET, pet)
+            }
     }
-  }
 
-  private val binding by lazy { ActivityFeedPetBinding.inflate(layoutInflater) }
-  private val feedPetViewModel by viewModels<FeedPetViewModel>()
+    private val binding by lazy { ActivityFeedPetBinding.inflate(layoutInflater) }
+    private val feedPetViewModel by viewModels<FeedPetViewModel>()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(binding.root)
-    setupUi()
-  }
+    private var horizontalPositionDifference = 0f
+    private val springForce by lazy {
+        SpringForce(0f).apply {
+            stiffness = SpringForce.STIFFNESS_MEDIUM
+            dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        }
+    }
 
-  private fun setupUi() {
-    val data = intent.getSerializableExtra(KEY_PET) as? Pet ?: return
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupUi()
+    }
 
-    feedPetViewModel.setPet(data)
-    feedPetViewModel.petData.observe(this, { petData ->
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupUi() {
+        val data = intent.getSerializableExtra(KEY_PET) as? Pet ?: return
 
-      if (petData != null) {
-        binding.feedPetToolbar.title = getString(R.string.feedPetTitle, petData.name)
-        binding.petImage.load(petData.image)
-      }
-    })
+        feedPetViewModel.setPet(data)
+        feedPetViewModel.petData.observe(this, { petData ->
 
-    // TODO physics animation
-  }
+            if (petData != null) {
+                binding.feedPetToolbar.title = getString(R.string.feedPetTitle, petData.name)
+                binding.petImage.load(petData.image)
+            }
+        })
+
+        val springChocolateCookie = SpringAnimation(
+            binding.chocolateCookie,
+            DynamicAnimation.TRANSLATION_X
+        ).setSpring(springForce)
+
+        val springCookie = SpringAnimation(
+            binding.cookie,
+            DynamicAnimation.TRANSLATION_X
+        ).setSpring(springForce)
+
+        binding.chocolateCookie.setOnTouchListener(buildTouchListener(springChocolateCookie))
+        binding.cookie.setOnTouchListener(buildTouchListener(springCookie))
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun buildTouchListener(springAnimation: SpringAnimation) =
+        View.OnTouchListener { view, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    horizontalPositionDifference = event.rawX - (view?.x ?: 0f)
+
+                    springAnimation.cancel()
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    view?.x = event.rawX - horizontalPositionDifference
+                }
+
+                MotionEvent.ACTION_UP -> springAnimation.start()
+            }
+
+            true
+        }
 }
